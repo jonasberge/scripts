d41748ac2f7b2f6b0ff92ece3673a5eae778a779
vonas <24623262+vonas@users.noreply.github.com>
2022-01-08 19:27:34 +0100

    Start gnirehtet activity on my work profile too

--- gnirehtet-serial-autorun/service.sh
+++ gnirehtet-serial-autorun/service.sh
@@ -5,6 +5,9 @@ source "$(dirname $0)/.env"
 while :; do
 	echo "[INFO] waiting for device $DEVICE_SERIAL..."
 	adb -s "$DEVICE_SERIAL" wait-for-device
+	# start gnirehtet for work profile user
+	adb shell am start --user 10 \
+		-a com.genymobile.gnirehtet.START -n com.genymobile.gnirehtet/.GnirehtetActivity
 	gnirehtet run -s "$DEVICE_SERIAL" -d "$DNS_SERVERS_CSV" &
 	adb -s "$DEVICE_SERIAL" wait-for-disconnect;
 	echo "[INFO] device $DEVICE_SERIAL disconnected."
